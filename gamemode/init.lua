98c338aa6b3a68ccd257c2c2fee2e5879b54712a
Cherrio <toddyaspringer@gmail.com>
2020-03-20 22:57:59 -0700

    Add a player base class, set model, get hands model.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -2,3 +2,14 @@ AddCSLuaFile( "cl_init.lua" ) --Tell the server that the client needs to downloa
 AddCSLuaFile( "shared.lua" ) --Tell the server that the client needs to download shared.lua
 
 include( 'shared.lua' ) --Tell the server to load shared.lua
+
+function GM:PlayerSpawn(ply)
+  player_manager.SetPlayerClass(ply , "player_custom")
+  player_manager.OnPlayerSpawn(ply)
+  player_manager.RunClass(ply, "Spawn")
+  ply:SetupHands()
+  hook.Call("PlayerLoadout", GAMEMODE, ply)
+  hook.Call("PlayerSetModel", GAMEMODE, ply)
+
+
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -5,3 +5,34 @@ GM.Author        = "Cherry :D"
 GM.Email        = "NO LOL"
 GM.Website        = "www.frostburnt.com"
 GM.TeamBased    = false
+
+DEFINE_BASECLASS( "player_default" )
+
+ local PLAYER = {}
+
+ --
+ -- See gamemodes/base/player_class/player_default.lua for all overridable variables
+ --
+ PLAYER.WalkSpeed = 200
+ PLAYER.RunSpeed  = 275
+
+ function PLAYER:Loadout()
+ 	self.Player:Give( "weapon_physcannon" )
+  self.Player:Give( "weapon_physgun" )
+  self.Player:Give( "gmod_tool" )
+  self.Player:Give( "gmod_camera" )
+
+ end
+
+ function PLAYER:SetModel()
+  self.Player:SetModel("models/player/charple.mdl")
+  self:GetHandsModel()
+ end
+
+ function PLAYER:GetHandsModel()
+  local playermodel = player_manager.TranslateToPlayerModelName( self.Player:GetModel() )
+	return player_manager.TranslatePlayerHands( playermodel )
+
+ end
+
+ player_manager.RegisterClass( "player_custom", PLAYER, "player_default" )
